b974bf0382fda0b134808bfad76fa8f59ece508a
Marcelo Amancio <marceloamancio@Diegos-MacBook-Pro.local>
2017-08-19 19:15:23 -0300

    uva10340 solved

--- acm_uva/10340/uva30340.py
+++ acm_uva/10340/uva30340.py
@@ -1,3 +1,8 @@
+# UVA10340 - All in All - Accepted
+# goo.gl/mwTYqv
+# @author: Marcelo Adriano Amancio
+# 'If you're trying to create a company, it's like baking a cake. You have to have all the ingredients in the right proportion.' -- Elon Musk 
+
 from sys import stdin
 
 def subseq(a,b):
